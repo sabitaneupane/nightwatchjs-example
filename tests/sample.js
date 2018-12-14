3657e4363bc570809724b67eb4deead2928577f3
Sabita Neupane <sabita.neupane@cloudfcatory.com>
2018-12-14 10:32:37 +0545

    Adding sample file

---
+++ tests/sample.js
@@ -0,0 +1,15 @@
+module.exports = {
+    'Searching nightwatch in youtube': function (browser) {
+      browser
+        .url("http://www.youtube.com/")
+        .pause(2000)
+        .setValue('#search' , "Nightwatch js")
+        .pause(2000)
+        .keys(browser.Keys.ENTER)
+        .pause(2000)
+    },
+    after: function (browser) {
+      browser
+        .end();
+    }
+  };
